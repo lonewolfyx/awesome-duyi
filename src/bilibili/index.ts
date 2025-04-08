c1541370c41f9918a30443539211715934631db3
lonewolfyx <877085893@qq.com>
2025-04-08 16:00:51 +0800

    fix: bilibili

--- src/bilibili/index.ts
+++ src/bilibili/index.ts
@@ -3,6 +3,10 @@ import axios from 'axios'
 import { getWebId } from '@/bilibili/webid.ts'
 import { getBiliBiliCookies } from '@/bilibili/cookie.ts'
 import { getBiliBiliRequestRid } from '@/bilibili/rid.ts'
+import fs from 'node:fs'
+import dayjs from 'dayjs'
+import { resolve } from 'node:path'
+import { cwd } from 'node:process'
 
 export const getBiliBili = async (channel: BiliBiliChannel) => {
 	const headers = {
@@ -14,15 +18,15 @@ export const getBiliBili = async (channel: BiliBiliChannel) => {
 		'sec-fetch-site': 'same-site',
 		'sec-fetch-mode': 'cors',
 		'sec-fetch-dest': 'empty',
-		referer: `https://space.bilibili.com/${channel.id}/video`
+		referer: `https://space.bilibili.com/${channel.mid}/video`
 	}
 
 	const cookies = await getBiliBiliCookies()
-	const webId = await getWebId(channel.id.toString())
+	const webId = await getWebId(channel.mid.toString())
 	const now = Math.round(Date.now() / 1e3)
 
 	const queryParams = {
-		mid: channel.id,
+		mid: channel.mid,
 		ps: 30,
 		tid: 0,
 		pn: 1,
@@ -54,17 +58,35 @@ export const getBiliBili = async (channel: BiliBiliChannel) => {
 	})
 
 	const videoList = res?.data?.data?.list?.vlist
-	videoList.map((row: any) => {
-		console.log(row.title)
-	})
-	return channel.id
-}
-;(async () => {
-	console.log(
-		await getBiliBili({
-			id: 384876532,
-			name: '',
-			avatar: ''
+	// videoList.map((row: any) => {
+	// 	console.log(row.title)
+	// })
+	//     const vList = JSON.parse(text)['data']['list']['vlist']
+
+	const filePath = resolve(cwd(), `./data/${channel.alias}.json`)
+	const row = videoList[0]
+	const oldContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+	const firstContent = oldContent[0]
+	const lastTime = row.created
+	const contentLastTime = dayjs(firstContent.time).valueOf() / 1e3
+	if (contentLastTime < lastTime) {
+		oldContent.unshift({
+			title: row.title,
+			url: `https://www.bilibili.com/${row.bvid}`,
+			time: dayjs.unix(row.created).format('YYYY-MM-DD HH:mm:ss'),
+			cover: row.pic
 		})
-	)
-})()
+		fs.writeFileSync(filePath, JSON.stringify(oldContent), {
+			encoding: 'utf8',
+			flag: 'w'
+		})
+	}
+}
+// ;(async () => {
+// 	await getBiliBili({
+// 		mid: 3494367522195464,
+// 		alias: 'qdbxk',
+// 		name: '渡一前端必修课',
+// 		url: 'https://space.bilibili.com/3494367522195464/video'
+// 	})
+// })()
