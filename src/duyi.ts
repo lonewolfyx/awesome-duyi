9475a04bf637ebe1a550dc796c3f423a4e800868
lonewolfyx <877085893@qq.com>
2025-04-08 15:28:36 +0800

    fix: playwright douyin

--- src/duyi.ts
+++ src/duyi.ts
@@ -1,32 +1,29 @@
-import { user, type UserList } from '@/user.ts'
-import { getBiliBiliDataList, getBiliBiliNewData } from '@/playwright/bilibili.ts'
+import { biliBiliUser, douYinUser } from '@/user.ts'
 import { getDouYinNewData } from '@/playwright/douyin.ts'
+import { getBiliBili } from '@/bilibili'
 
-async function main(users: UserList[]) {
-	for (const item of users) {
-		if (item.type === 'douyin') {
-			// 计算当前这次的延迟
-			const delay = 2000
+const runDouYin = async () => {
+	for (const item of douYinUser) {
+		// 计算当前这次的延迟
+		const delay = 2000
 
-			// 等待当前的总延迟时间
-			await new Promise((resolve) => setTimeout(resolve, delay))
+		// 等待当前的总延迟时间
+		await new Promise((resolve) => setTimeout(resolve, delay))
 
-			await getDouYinNewData(item.id, item.url)
-		}
-
-		if (item.type === 'bilibili') {
-			// 计算当前这次的延迟
-			const delay = 2000
-
-			// 等待当前的总延迟时间
-			await new Promise((resolve) => setTimeout(resolve, delay))
-
-			// await getBiliBiliDataList(item.id, item.url)
-			await getBiliBiliNewData(item.id, item.url)
-		}
+		await getDouYinNewData(item.id, item.url)
 	}
 }
 
-;(async () => {
-	await main(user)
-})()
+const runBiliBili = async () => {
+	for (const item of biliBiliUser) {
+		// 计算当前这次的延迟
+		const delay = 2000
+
+		// 等待当前的总延迟时间
+		await new Promise((resolve) => setTimeout(resolve, delay))
+
+		await getBiliBili(item)
+	}
+}
+
+;(async () => await runDouYin())()
--- src/playwright/douyin.ts
+++ src/playwright/douyin.ts
@@ -1,51 +1,99 @@
 import { chromium } from 'playwright'
-import * as fs from 'node:fs'
-import { cwd } from 'node:process'
 import { resolve } from 'node:path'
+import { cwd } from 'node:process'
+import fs, { readFileSync, writeFileSync } from 'node:fs'
 import dayjs from 'dayjs'
 
 export const getDouYinNewData = async (fileName: string, url: string) => {
 	const browser = await chromium.launch({
-		headless: true
+		headless: false
 	})
-	const context = await browser.newContext()
+	const cookiePath = resolve(cwd(), 'storageState.json')
+	console.log(cookiePath)
+	const context = await browser.newContext({
+		storageState: JSON.parse(readFileSync(cookiePath, 'utf-8'))
+	})
 	const page = await context.newPage()
 
 	const filePath = resolve(cwd(), `./data/${fileName}.json`)
+	fs.writeFileSync(filePath, '[\n', {
+		encoding: 'utf8',
+		flag: 'w'
+	})
 
 	page.on('response', async (response) => {
 		if (response.url().includes('aweme/v1/web/aweme/post')) {
 			const text = await response.text()
-			if (text && JSON.parse(text)?.aweme_list?.[0]) {
-                console.log(text)
-				// const video = JSON.parse(text)['aweme_list'][0]
-				// const lastTime = video.create_time
-                //
-				// const oldContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-				// const firstContent = oldContent[0]
-				// const contentLastTime = dayjs(firstContent.time).valueOf() / 1e3
-                //
-				// if (contentLastTime < lastTime) {
-				// 	oldContent.unshift({
-				// 		title: video.item_title || video.desc.split('\n')[0],
-				// 		url: `https://www.douyin.com/video/${video.aweme_id}`,
-				// 		time: dayjs.unix(lastTime).format('YYYY-MM-DD HH:mm:ss')
-				// 	})
-				// 	fs.writeFileSync(filePath, JSON.stringify(oldContent), {
-				// 		encoding: 'utf8',
-				// 		flag: 'w'
-				// 	})
-				// }
+			if (text && JSON.parse(text)?.aweme_list) {
+				const video = JSON.parse(text)['aweme_list']
+
+				video.forEach((row: any) => {
+					const title = row.item_title || row.desc.split('\n')[0]
+					console.log(title)
+					const url = `https://www.douyin.com/video/${row.aweme_id}`
+					const time = dayjs.unix(row.create_time).format('YYYY-MM-DD HH:mm:ss')
+					const cover = row.video?.dynamic_cover?.url_list[0] || row.video?.origin_cover?.url_list[0] || ''
+					const content = `{"title":"${title}", "url":"${url}","time":"${time}","cover":"${cover}"},\n`
+					fs.writeFileSync(filePath, content, {
+						encoding: 'utf8',
+						flag: 'a'
+					})
+				})
 			}
+
+			// if (text && JSON.parse(text)?.aweme_list?.[0]) {
+			//     console.log(text)
+			//     const video = JSON.parse(text)['aweme_list'][0]
+			//     const lastTime = video.create_time
+			//
+			//     const oldContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+			//     const firstContent = oldContent[0]
+			//     const contentLastTime = dayjs(firstContent.time).valueOf() / 1e3
+			//
+			//     if (contentLastTime < lastTime) {
+			// const cover = row.video?.dynamic_cover?.url_list[0] || row.video?.origin_cover?.url_list[0] || ''
+			//     	oldContent.unshift({
+			//     		title: video.item_title || video.desc.split('\n')[0],
+			//     		url: `https://www.douyin.com/video/${video.aweme_id}`,
+			//     		time: dayjs.unix(lastTime).format('YYYY-MM-DD HH:mm:ss')
+			//     	})
+			//     	fs.writeFileSync(filePath, JSON.stringify(oldContent), {
+			//     		encoding: 'utf8',
+			//     		flag: 'w'
+			//     	})
+			//     }
+			// }
 		}
 	})
 
 	await page.goto(url)
 
 	// 等待登录框出现
-	await page.waitForSelector('._CwbZHXv', { timeout: 20000 })
-	// 关闭登录框
-	await page.locator('._CwbZHXv > svg').click()
+	// await page.waitForSelector('._CwbZHXv', { timeout: 20000 })
+
+	// // 关闭登录框
+	// await page.locator('._CwbZHXv > svg').click()
+	//
+
+	let status = true
+	while (status) {
+		const pageText = await page.locator('.B_mbw29p').count()
+		console.log(pageText)
+		await page.waitForTimeout(2000)
+		// 触发 end 键盘事件，页面滚动至底部
+		await page.keyboard.press('End')
+		if (pageText) {
+			status = false
+		}
+	}
+
+	fs.writeFileSync(filePath, ']\n', {
+		encoding: 'utf8',
+		flag: 'a'
+	})
+
+	const newCookies = await context.storageState()
+	writeFileSync(cookiePath, JSON.stringify(newCookies, null, 2))
 
 	// 等待 2 秒后关闭
 	setTimeout(async () => {
@@ -53,7 +101,3 @@ export const getDouYinNewData = async (fileName: string, url: string) => {
 		await browser.close()
 	}, 2000)
 }
-
-(async ()=>{
-    await getDouYinNewData('txk','https://www.douyin.com/user/MS4wLjABAAAAeIIkCgELXG6XdUxuE9nQ6W4AfS-aoPFbtmnBL8ytcYtBSyurgePBYZXJpB0LJBCT')
-})()
