53f1ea96cd7535d7b3d835b14bba6d32407dd08f
lonewolfyx <877085893@qq.com>
2025-12-02 09:28:01 +0800

    feat(docs): add the navigation generation function and refactor the configuration

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,13 @@
+import type { BaseChannel, ChannelType } from '../../types/types'
 import { defineConfig } from 'vitepress'
+import { biliBiliUser, douYinUser } from '../../src/user'
+
+function generatorNav(channel: BaseChannel[], type: ChannelType) {
+    return channel.map((item: BaseChannel) => ({
+        text: item.name,
+        link: `/${type}/${item.vpName}`,
+    }))
+}
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -27,27 +36,13 @@ export default defineConfig({
             '/douyin/': [
                 {
                     text: '抖音专辑',
-                    items: [
-                        { text: '渡一前端提薪课', link: '/douyin/salary_increase_course' },
-                        { text: '渡一Web前端学习频道', link: '/douyin/learning_channel' },
-                        { text: '渡一前端教科书', link: '/douyin/textbook' },
-                        { text: '渡一前端必修课', link: '/douyin/compulsory_course' },
-                        { text: '渡一前端讲书频道', link: '/douyin/book_talk_channel' },
-                        { text: '渡一前端代码优化CodeReview', link: '/douyin/code_review' },
-                    ],
+                    items: generatorNav(douYinUser, 'douyin'),
                 },
             ],
             '/bilibili/': [
                 {
                     text: '哔哩哔哩专辑',
-                    items: [
-                        { text: '渡一机构', link: '/bilibili/institution' },
-                        { text: '渡一教育-前端进阶课', link: '/bilibili/front_end_advanced' },
-                        { text: '渡一教育编程课堂', link: '/bilibili/programming' },
-                        { text: '渡一前端提薪空间', link: '/bilibili/salary_increase_course' },
-                        { text: '渡一教育-Web前端开发', link: '/bilibili/front_dev' },
-                        { text: '渡一前端必修课', link: '/bilibili/compulsory_course' },
-                    ],
+                    items: generatorNav(biliBiliUser, 'bilibili'),
                 },
             ],
         },
